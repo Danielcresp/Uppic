2214db673b59264e93db4f61e5b7dd1f5d49df3b
Danielcresp <dneli6cres@outlook.com>
2019-03-30 22:57:16 -0600

    Uso de watchify para recompilar automaticamente

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 gulp.task('style', done => {
     gulp.src('index.scss')
         .pipe(sass())
@@ -16,15 +17,42 @@ gulp.task('assets',done=>{
          .pipe( gulp.dest('public'));
          done();
 })
-gulp.task('scripts',done=>{
-     browserify('./src/index.js')
-    .transform(babelify)
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('public'))
+
+function compile(watch){
+    var bundle = watchify(browserify('./src/index.js'));
+    function rebundle(){
+        bundle
+        .transform(babelify)
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+    }
+    if(watch){
+        bundle.on('update',function(){
+            console.log('--> Bundling...');
+            rebundle();
+        })
+    }
+    rebundle();
+}
+// gulp.task('scripts',done=>{
+//      browserify('./src/index.js')
+//     .transform(babelify)
+//     .bundle()
+//     .pipe(source('index.js'))
+//     .pipe(rename('app.js'))
+//     .pipe(gulp.dest('public'))
+//     done();
+// })
+gulp.task('build',done=>{
+    return compile();
     done();
-})
-gulp.task('default', gulp.series('style','assets','scripts'));
+});
+gulp.task('watch',done =>{
+    return compile(true);
+    done();
+});
+gulp.task('default', gulp.series('style','assets','build'));
 
 
--- public/app.js
+++ public/app.js
@@ -1403,11 +1403,11 @@ process.umask = function() { return 0; };
 var page = require('page');
 var main = document.getElementById('main-container');
 page('/', function (ctx, next) {
-    main.innerHTML = 'Home <a href="/signup">Signup</a>'; //Home
+    main.innerHTML = 'Home Page<a href="/signup">Signup</a>'; //Home
 });
 
 page('/signup', function (ctx, next) {
-    main.innerHTML = 'SignUp <a href="/">Home</a>'; //SIgnUp
+    main.innerHTML = 'SignUp 2<a href="/">Home Page</a>'; //SIgnUp
 });
 // page();
 page.start();
