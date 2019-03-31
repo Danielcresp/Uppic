4f9fa3dce4dea822ff0e6c5f991829bd5c5231af
Danielcresp <dneli6cres@outlook.com>
2019-03-31 07:00:31 -0600

    Usar moulos para cambiar formulario sin recargar

--- public/app.js
+++ public/app.js
@@ -2845,56 +2845,116 @@ module.exports = [
 
 },{}],15:[function(require,module,exports){
 var page = require('page');
-var empty = require('empty-element');
-var yo = require('yo-yo');
-var main = document.getElementById('main-container');
 page('/', function (ctx, next) {
-    //Home
-    main.innerHTML = '<a href="/signup">Signup</a>';
+    var main = document.getElementById('main-container');
 });
 
-page('/signup', function (ctx, next) {
-    //SIgnUp
-    var el = yo`<div class="container">
-   <div class="row">
-       <div class="col s10 push-s1">
-           <div class="row">
-               <div class="col m5 hide-on-small-only">
-                   <img src="phone.png" alt="" class="iphone">
-               </div>
-               <div class="col s12 m7">
-                   <div class="row">
-                       <div class="signup-box">
-                               <h1 class="uppic">Uppic</h1>
-                               <form action="" class="signup-form">
-                                   <h2>Registrate para ver fotos de tu red Uppic</h2>
-                                   <div class="section">
-                                       <a href="" class="btn btn-fb hide-on-small-only">Sesión con Facebook</a>
-                                       <a href="" class="btn btn-go hide-on-small-only">Sesión con Google</a>
-                                       <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
-                                   </div>
-                                   <div class="divider"></div>
-                                   <div class="section">
-                                       <input type="email" name="email" paceholder="Email" id="email" class="inputs"/>
-                                       <input type="text" name="name" paceholder="Name" id="name" class="inputs"/>
-                                       <input type="text" name="usename" paceholder="usename" id="username" class="inputs"/>
-                                       <input type="password" name="password" paceholder="Password" id="password" class="inputs"/>
-                                       <input type="submit" class="btn btn-signup waves-effect wave-light"  value="Registrate"/>
-                                   </div>
-                               </form>
-                       </div>
-                       <div class="login-box">
-                           ¿Tienes una cuenta? <a href="http://singin">Entrar</a>
-                       </div>
-                   </div>         
-               </div>
-           </div>
-       </div>
-   </div>
-</div>`;
-    empty(main).appendChild(el);
-});
-// page();
+},{"page":11}],16:[function(require,module,exports){
+var page = require('page');
+require('./homepage');
+require('./signup');
+require('./signin');
+
 page.start();
 
-},{"empty-element":3,"page":11,"yo-yo":13}]},{},[15]);
+},{"./homepage":15,"./signin":18,"./signup":20,"page":11}],17:[function(require,module,exports){
+var yo = require('yo-yo');
+
+module.exports = function landing(box) {
+    return yo`<div class="container">
+<div class="row">
+    <div class="col s10 push-s1">
+        <div class="row">
+            <div class="col m5 hide-on-small-only">
+                    <img src="phone.png" alt="" class="iphone">
+            </div>
+            ${box}
+        </div>
+    </div>
+</div>`;
+};
+
+},{"yo-yo":13}],18:[function(require,module,exports){
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+
+page('/signin', function (ctx, next) {
+    var main = document.getElementById('main-container');
+    empty(main).appendChild(template);
+});
+page();
+
+},{"./template":19,"empty-element":3,"page":11}],19:[function(require,module,exports){
+var yo = require('yo-yo');
+var landing = require('../landing');
+var signinForm = yo`<div class="col s12 m7">
+<div class="row">
+    <div class="signup-box">
+            <h1 class="uppic">Uppic</h1>
+            <form action="" class="signup-form">
+                <h2>Registrate para ver fotos de tu red Uppic</h2>
+                <div class="section">
+                    <a href="" class="btn btn-fb hide-on-small-only">Sesión con Facebook</a>
+                    <a href="" class="btn btn-go hide-on-small-only">Sesión con Google</a>
+                    <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+                </div>
+                <div class="divider"></div>
+                <div class="section">
+                    <input type="email" name="email" paceholder="Email" id="email" class="inputs"/>
+                    <input type="text" name="name" paceholder="Name" id="name" class="inputs"/>
+                    <input type="text" name="usename" paceholder="usename" id="username" class="inputs"/>
+                    <input type="password" name="password" paceholder="Password" id="password" class="inputs"/>
+                    <input type="submit" class="btn btn-signup waves-effect wave-light"  value="Registrate"/>
+                </div>
+            </form>
+    </div>
+    <div class="login-box">
+        ¿Tienes una cuenta? <a href="/signup">Entrar</a>
+    </div>
+</div>         
+</div>
+</div>`;
+module.exports = landing(signinForm);
+
+},{"../landing":17,"yo-yo":13}],20:[function(require,module,exports){
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+
+page('/signup', function (ctx, next) {
+    var main = document.getElementById('main-container');
+    empty(main).appendChild(template);
+});
+page();
+
+},{"./template":21,"empty-element":3,"page":11}],21:[function(require,module,exports){
+var yo = require('yo-yo');
+var landing = require('../landing');
+var signupForm = yo`<div class="col s12 m7">
+<div class="row">
+    <div class="signup-box">
+            <h1 class="uppic">Uppic</h1>
+            <form action="" class="signup-form">
+                <div class="section">
+                    <a href="" class="btn btn-fb hide-on-small-only">Sesión con Facebook</a>
+                    <a href="" class="btn btn-go hide-on-small-only">Sesión con Google</a>
+                    <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+                </div>
+                <div class="divider"></div>
+                <div class="section">
+                    <input type="text" name="usename" paceholder="usename" id="username" class="inputs"/>
+                    <input type="password" name="password" paceholder="Password" id="password" class="inputs"/>
+                    <input type="submit" class="btn btn-signup waves-effect wave-light"  value="Inicia Sesion"/>
+                </div>
+            </form>
+    </div>
+    <div class="login-box">
+        ¿No tienes una cuenta? <a href="/signin">Registrate</a>
+    </div>
+</div>         
+</div>
+</div>`;
+module.exports = landing(signupForm);
+
+},{"../landing":17,"yo-yo":13}]},{},[16]);
--- src/signin/template.js
+++ src/signin/template.js
@@ -1,10 +1,11 @@
 var yo = require('yo-yo');
 var landing = require('../landing')
-var signupForm = yo`<div class="col s12 m7">
+var signinForm= yo`<div class="col s12 m7">
 <div class="row">
     <div class="signup-box">
             <h1 class="uppic">Uppic</h1>
             <form action="" class="signup-form">
+                <h2>Registrate para ver fotos de tu red Uppic</h2>
                 <div class="section">
                     <a href="" class="btn btn-fb hide-on-small-only">Sesión con Facebook</a>
                     <a href="" class="btn btn-go hide-on-small-only">Sesión con Google</a>
@@ -12,17 +13,18 @@ var signupForm = yo`<div class="col s12 m7">
                 </div>
                 <div class="divider"></div>
                 <div class="section">
+                    <input type="email" name="email" paceholder="Email" id="email" class="inputs"/>
+                    <input type="text" name="name" paceholder="Name" id="name" class="inputs"/>
                     <input type="text" name="usename" paceholder="usename" id="username" class="inputs"/>
                     <input type="password" name="password" paceholder="Password" id="password" class="inputs"/>
-                    <input type="submit" class="btn btn-signup waves-effect wave-light"  value="Inicia Sesion"/>
+                    <input type="submit" class="btn btn-signup waves-effect wave-light"  value="Registrate"/>
                 </div>
             </form>
     </div>
     <div class="login-box">
-        ¿No tienes una cuenta? <a href="/signin">Registrate</a>
+        ¿Tienes una cuenta? <a href="/signup">Entrar</a>
     </div>
 </div>         
 </div>
 </div>`;
-module.exports = landing(signupForm);
-
+module.exports =landing(signinForm);
